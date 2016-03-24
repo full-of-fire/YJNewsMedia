3381131e031d0080573ae3ffa2012c652a9e24bd
yj <yj@yjdemac-mini.local>
2016-03-24 14:35:41 +0800

    Initial Commit

---
+++ 鲜闻基础架构-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  鲜闻基础架构-Demo
+//
+//  Created by yj on 16/3/24.
+//  Copyright © 2016年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 鲜闻基础架构-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  鲜闻基础架构-Demo
+//
+//  Created by yj on 16/3/24.
+//  Copyright © 2016年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
